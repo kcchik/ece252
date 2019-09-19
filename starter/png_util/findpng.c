7bcef000aec7ff193ee1d5d18d30b2fb2b10839d
Yu Xuan Ou <yx2ou@uwaterloo.ca>
2019-09-19 11:21:52 -0400

    removed comments from findpng

--- starter/png_util/findpng.c
+++ starter/png_util/findpng.c
@@ -47,40 +47,5 @@ int main (int argc, char **argv)
     if (pngCount < 1){
         printf("findpng: No PNG file found\n");
     }
-    // FILE *fp = fopen(argv[1], "rb");
-    // if (fp == NULL) {
-    //     printf("Unable to open file");
-    //     return -1;
-    // }
-    // if (!is_png(fp)) {
-    //     printf("%s: Not a PNG file\n", argv[1]);
-    //     fclose(fp);
-    //     return -1;
-    // }
-
-    // struct simple_PNG png;
-    // struct chunk ihdr;
-    // struct chunk idat;
-    // struct chunk iend;
-    // png.p_IHDR = ihdr;
-    // png.p_IDAT = idat;
-    // png.p_IEND = iend;
-    // get_chunks(&png, fp);
-
-    // if (!validate_CRC(&png.p_IHDR)) {
-    //     fclose(fp);
-    //     return -1;
-    // }
-
-    // struct data_IHDR data_ihdr;
-    // get_IHDR(&data_ihdr, png.p_IHDR, fp);
-    // printf("%s: %d x %d\n", argv[1], data_ihdr.width, data_ihdr.height);
-
-    // if (!validate_CRC(&png.p_IDAT) || !validate_CRC(&png.p_IEND)) {
-    //     fclose(fp);
-    //     return -1;
-    // }
-
-    // fclose(fp);
     return 0;
 }
