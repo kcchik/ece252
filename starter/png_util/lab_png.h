47317d4f4d66a6f312a0c6d7980ed181eca040e5
Kevin Chik <kcchik@v1040-wn-rt-a-114-77.campus-dynamic.uwaterloo.ca>
2019-09-19 08:35:00 -0400

    init

--- starter/png_util/lab_png.h
+++ starter/png_util/lab_png.h
@@ -13,26 +13,15 @@
 #include <stdio.h>
 
 /******************************************************************************
- * DEFINED MACROS
- *****************************************************************************/
-
-#define PNG_SIG_SIZE    8 /* number of bytes of png image signature data */
-#define CHUNK_LEN_SIZE  4 /* chunk length field size in bytes */
-#define CHUNK_TYPE_SIZE 4 /* chunk type field size in bytes */
-#define CHUNK_CRC_SIZE  4 /* chunk CRC field size in bytes */
-#define DATA_IHDR_SIZE 13 /* IHDR chunk data field size */
-
-/******************************************************************************
  * STRUCTURES and TYPEDEFS
  *****************************************************************************/
-typedef unsigned char U8;
 typedef unsigned int  U32;
 
 typedef struct chunk {
     U32 length;  /* length of data in the chunk, host byte order */
     U8  type[4]; /* chunk type */
     U8  *p_data; /* pointer to location where the actual data are */
-    U32 crc;     /* CRC field  */
+    U32  crc;    /* CRC field  */
 } *chunk_p;
 
 /* note that there are 13 Bytes valid data, compiler will padd 3 bytes to make
@@ -53,21 +42,107 @@ typedef struct data_IHDR {// IHDR chunk data
 
 /* A simple PNG file format, three chunks only*/
 typedef struct simple_PNG {
-    struct chunk *p_IHDR;
-    struct chunk *p_IDAT;  /* only handles one IDAT chunk */
-    struct chunk *p_IEND;
+    struct chunk p_IHDR;
+    struct chunk p_IDAT;  /* only handles one IDAT chunk */
+    struct chunk p_IEND;
 } *simple_PNG_p;
 
 /******************************************************************************
  * FUNCTION PROTOTYPES
  *****************************************************************************/
-int is_png(U8 *buf, size_t n);
-int get_png_height(struct data_IHDR *buf);
-int get_png_width(struct data_IHDR *buf);
-int get_png_data_IHDR(struct data_IHDR *out, FILE *fp, long offset, int whence);
+int is_png(FILE *fp) {
+    U8 *buf = malloc(8);
+    fread(buf, sizeof(buf), 1, fp);
+    if (
+        buf[0] == 0x89 &&
+        buf[1] == 0x50 &&
+        buf[2] == 0x4e &&
+        buf[3] == 0x47 &&
+        buf[4] == 0x0d &&
+        buf[5] == 0x0a &&
+        buf[6] == 0x1a &&
+        buf[7] == 0x0a
+    ) {
+        free(buf);
+        return 1;
+    }
+    free(buf);
+    return 0;
+}
 
-/* declare your own functions prototypes here */
+int get_chunk(struct chunk *out, FILE *fp, long offset) {
+    U8 *buf = malloc(4);
+    fseek(fp, offset, SEEK_SET);
+    fread(buf, sizeof(buf), 1, fp);
+    int len;
+    for (int i = 0; i < 4; i++) {
+        len = len * 256 + buf[i];
+    }
+    out->length = len;
 
-int is_png(U8 *buf, size_t n) {
-    return buf == (U8*) 0x58904E470D0A1A0A;
+    fseek(fp, offset + 4, SEEK_SET);
+    fread(buf, sizeof(buf), 1, fp);
+    for (int i = 0; i < sizeof(buf); i++) {
+        out->type[i] = buf[i];
+    }
+
+    U8 *stream = malloc(len);
+    fseek(fp, offset + 8, SEEK_SET);
+    fread(stream, len, 1, fp);
+    out->p_data = stream;
+
+    fseek(fp, offset + len + 8, SEEK_SET);
+    fread(buf, sizeof(buf), 1, fp);
+    int crc;
+    for (int i = 0; i < 4; i++) {
+        crc = crc * 256 + buf[i];
+    }
+    out->crc = crc;
+
+    free(buf);
+    return 1;
+}
+
+int get_chunks(struct simple_PNG *out, FILE *fp) {
+    get_chunk(&out->p_IHDR, fp, 8);
+    get_chunk(&out->p_IDAT, fp, out->p_IHDR.length + 20);
+    get_chunk(&out->p_IEND, fp, out->p_IHDR.length + out->p_IDAT.length + 32);
+    return 1;
+}
+
+int get_IHDR(struct data_IHDR *out, struct chunk ihdr, FILE *fp) {
+    int w, h;
+    for (int i = 0; i < 4; i++) {
+        w = w * 256 + ihdr.p_data[i];
+    }
+    for (int i = 4; i < 8; i++) {
+        h = h * 256 + ihdr.p_data[i];
+    }
+    out->width = w;
+    out->height = h;
+    out->bit_depth = ihdr.p_data[8];
+    out->color_type = ihdr.p_data[9];
+    out->compression = ihdr.p_data[10];
+    out->filter = ihdr.p_data[11];
+    out->interlace = ihdr.p_data[12];
+    return 1;
+}
+
+int validate_CRC(struct chunk *chunk) {
+    U8 *stream = malloc(chunk->length + 4);
+    for (int i = 0; i < 4; i++) {
+        stream[i] = chunk->type[i];
+    }
+    for (int i = 0; i < chunk->length; i++) {
+        int j = i + 4;
+        stream[j] = chunk->p_data[i];
+    }
+    unsigned long x = crc(stream, chunk->length + 4);
+    if (x != chunk->crc) {
+        printf("%.4s chunk CRC error: computed %04x, expected %04x\n", chunk->type, x, chunk->crc);
+        free(stream);
+        return 0;
+    }
+    free(stream);
+    return 1;
 }
--- starter/png_util/main.c
+++ starter/png_util/main.c
@@ -48,67 +48,40 @@ void init_data(U8 *buf, int len)
 
 int main (int argc, char **argv)
 {
-    // U8 *p_buffer = NULL;  /* a buffer that contains some data to play with */
-    // U32 crc_val = 0;      /* CRC value                                     */
-    // int ret = 0;          /* return value for various routines             */
-    // U64 len_def = 0;      /* compressed data length                        */
-    // U64 len_inf = 0;      /* uncompressed data length                      */
+    FILE *fp = fopen(argv[1], "rb");
+    if (fp == NULL) {
+        printf("Unable to open file");
+        return -1;
+    }
+    if (!is_png(fp)) {
+        printf("%s: Not a PNG file\n", argv[1]);
+        fclose(fp);
+        return -1;
+    }
 
-    // /* Step 1: Initialize some data in a buffer */
-    // /* Step 1.1: Allocate a dynamic buffer */
-    // p_buffer = malloc(BUF_LEN);
-    // if (p_buffer == NULL) {
-    //     perror("malloc");
-	// return errno;
-    // }
+    struct simple_PNG png;
+    struct chunk ihdr;
+    struct chunk idat;
+    struct chunk iend;
+    png.p_IHDR = ihdr;
+    png.p_IDAT = idat;
+    png.p_IEND = iend;
+    get_chunks(&png, fp);
 
-    // /* Step 1.2: Fill the buffer with some data */
-    // init_data(p_buffer, BUF_LEN);
+    if (!validate_CRC(&png.p_IHDR)) {
+        fclose(fp);
+        return -1;
+    }
 
-    // /* Step 2: Demo how to use zlib utility */
-    // ret = mem_def(gp_buf_def, &len_def, p_buffer, BUF_LEN, Z_DEFAULT_COMPRESSION);
-    // if (ret == 0) { /* success */
-    //     printf("original len = %d, len_def = %lu\n", BUF_LEN, len_def);
-    // } else { /* failure */
-    //     fprintf(stderr,"mem_def failed. ret = %d.\n", ret);
-    //     return ret;
-    // }
+    struct data_IHDR data_ihdr;
+    get_IHDR(&data_ihdr, png.p_IHDR, fp);
+    printf("%s: %d x %d\n", argv[1], data_ihdr.width, data_ihdr.height);
 
-    // ret = mem_inf(gp_buf_inf, &len_inf, gp_buf_def, len_def);
-    // if (ret == 0) { /* success */
-    //     printf("original len = %d, len_def = %lu, len_inf = %lu\n", \
-    //            BUF_LEN, len_def, len_inf);
-    // } else { /* failure */
-    //     fprintf(stderr,"mem_def failed. ret = %d.\n", ret);
-    // }
+    if (!validate_CRC(&png.p_IDAT) || !validate_CRC(&png.p_IEND)) {
+        fclose(fp);
+        return -1;
+    }
 
-    // /* Step 3: Demo how to use the crc utility */
-    // crc_val = crc(gp_buf_def, len_def); // down cast the return val to U32
-    // printf("crc_val = %u\n", crc_val);
-
-    // /* Clean up */
-    // free(p_buffer); /* free dynamically allocated memory */
-
-
-
-
-
-
-    // FILE *fp;
-    // U8 *buf = NULL;
-    // buf = malloc(8 * sizeof(U8));
-
-    // /* Open file for both reading and writing */
-    // fp = fopen("../images/red-green-16x16.png", "r");
-
-    // /* Seek to the beginning of the file */
-    // fseek(fp, 0, SEEK_SET);
-
-    // /* Read and display data */
-    // fread(buf, sizeof(U8), 1, fp);
-    // printf("%s\n", buf);
-    // fclose(fp);
-
-    // return(0);
-    test();
+    fclose(fp);
+    return 0;
 }
